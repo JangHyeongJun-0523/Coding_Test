a625cbf14065707217dd96567ff01dde5eac6032
Jang Hyung Jun <70615068+JangHyeongJun-0523@users.noreply.github.com>
2025-04-16 09:39:51 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+
+void PrintStars(int lines) {
+    for (int number_of_stars = 1; number_of_stars <= lines; ++number_of_stars) {
+        for (int iteration = 1; iteration <= number_of_stars; ++iteration) {
+            std::cout << '*';
+        }
+        std::cout << '\n';
+    }
+}
+
+
+int main() {
+    int lines = 0;
+    std::cin >> lines;
+    PrintStars(lines);
+
+    return 0;
+}
