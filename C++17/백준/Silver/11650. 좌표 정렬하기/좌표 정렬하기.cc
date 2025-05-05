e1cdecb2fae4b020133012989cc89400fe9139f6
Jang Hyung Jun <70615068+JangHyeongJun-0523@users.noreply.github.com>
2025-05-05 21:15:08 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 40 ms, Memory: 3688 KB -BaekjoonHub

---
+++ C++17/백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+
+    //input
+    int N;
+    std::cin >> N;
+    std::vector<std::pair<int, int>> points;
+    for (int i = 0; i < N; ++i) {
+        std::pair<int, int> point;
+        std::cin >> point.first >> point.second;
+        points.push_back(point);
+    }
+
+    //STL sort
+    std::sort(points.begin(), points.end());
+    
+    //output
+    for (int i = 0; i < N; ++i) {
+        std::cout << points[i].first << ' ' << points[i].second << '\n';
+    }
+    return 0;
+}
