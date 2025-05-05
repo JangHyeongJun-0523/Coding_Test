0759c1b25772ded4d55192fb663bb5765db83ccd
Jang Hyung Jun <70615068+JangHyeongJun-0523@users.noreply.github.com>
2025-05-05 15:20:55 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    int a, b, c, d, e, f;
+    std::cin >> a >> b >> c >> d >> e >> f;
+    for(int x = -999; x <= 999; ++x) {
+        for (int y = -999; y <= 999; ++y) {
+            if (a*x + b*y == c) {
+                if (d*x + e*y == f) {
+                    std::cout << x << ' ' << y << std::endl;
+                    break;
+                }
+            }
+        }
+    }
+    return 0;
+}
